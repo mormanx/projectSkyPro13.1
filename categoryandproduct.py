45cd9d0dbb1f3dea6ac161fa37eaedf9d2f92f64
alex <forester110011@gmail.com>
2024-04-07 21:19:40 +1000

    fix mistakes

--- categoryandproduct.py
+++ categoryandproduct.py
@@ -4,12 +4,14 @@ class Category():
     product: list
     total_categories = 0
     total_unique_products = 0
-    def __init__(self, name, description):
+    def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.__products = []
+        self.__products = products
 
         Category.total_categories += 1
+        unique_products = set(self.__products)
+        Category.total_unique_products = len(unique_products)
 
 
     def add_product(self, product):
@@ -33,16 +35,16 @@ class Category():
 class Product():
     name: str
     description: str
-    price: float
+    __price: float
     count_in_stock = int
     def __init__(self, name, description, price, count_in_stock):
         self.name = name
         self.description = description
-        self.price = float(price)
+        self.__price = float(price)
         self.count_in_stock = int(count_in_stock)
     @classmethod
-    def create_product(cls, name, description, price, count_in_stock):
-        return cls(name, description, price, count_in_stock)
+    def create_product(cls, **kwargs):
+        return cls(**kwargs)
     @property
     def price(self):
         return self.__price
@@ -53,22 +55,19 @@ class Product():
             print('Цена введена некорректно!')
         else:
             self.__price = value
-one = Category('Electronics', 'Electronic devices')
+# Создание продуктов
 two = Product('mouse', 'wireless mouse', 32.2, 2)
-four = Product('keybord', 'wireless keybord', 2222.2, 6)
+four = Product('keyboard', 'wireless keyboard', 2222.2, 6)
 three = Product('test1', 'test2', 55.5, 7)
 five = Product('test3', 'test4', 32.2, 2)
-one.add_product(two)
-one.add_product(three)
-one.add_product(four)
-one.add_product(five)
-print(one.products)
-# Остальной код...
+
+# Создание категории с продуктами
+one = Category('Electronics', 'Electronic devices', [two, three, four, five])
 
 print(one.products)
 
 # Проверка сеттера price
-two.price = 50.2 # Изменение цены на корректное значение
+two.price = 50.2  # Изменение цены на корректное значение
 print(f"Новая цена для {two.name}: {two.price}")
 
 two.price = -5.0  # Попытка установить некорректное значение цены
